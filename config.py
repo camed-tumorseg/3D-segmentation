9b484a70521691ee9d8563380eded03ca92cfc40
camed-tumorseg <camed.tumorsegmentierung@gmail.com>
2021-03-30 14:29:20 +0200

    Added model ensembling and cell for joining multiple training history files

--- config.py
+++ config.py
@@ -34,10 +34,18 @@ continue_training = True
 make_new_splits = False
 export_images = False
 
+kfold_validation = True
+
 #model_dim = (128, 128, 128)
 #model_dim = (240, 240, 160)
 model_dim = (160, 192, 128)
 
+
 ### EVALUATION
-weight_folder = "/mnt/Data/Vox2vox_output/models/Vox2Vox_192_epoch58/output"
+weight_folder = "/mnt/Data/Vox2vox_output/models/Vox2Vox_192_bs8_epoch100/output"
 eval_export_path = "export"
+
+
+### ENSEMBLE
+use_ensemble = False
+ensemble_folder = "/mnt/Data/Vox2vox_output/ensemble"
